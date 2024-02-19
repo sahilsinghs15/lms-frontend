7f70ed247086c525d2eeb8d59dd79b914f9ab8b7
sahil singh <sahilsingh1413@gmail.com>
2024-02-19 16:42:23 +0530

    Added Eslint config and React Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+import App from './App.jsx';
+import './index.css';
+import {BrowserRouter} from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+    
+  <BrowserRouter>
     <App />
+    <Toaster/>  
+  </BrowserRouter>
+    
 
 )
