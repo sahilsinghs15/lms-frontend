df69d5adb76ada9d4cf298543e1ac873d38debaf
sahil singh <sahilsingh1413@gmail.com>
2024-03-04 00:26:40 +0530

    Cleaned About Us page

---
+++ src/Constants/Celebrity.jsx
@@ -0,0 +1,46 @@
+import apj from "../Assets/Images/apj.png";
+import billGates from "../Assets/Images/billGates.png";
+import einstein from "../Assets/Images/einstein.png";
+import nelsonMandela from "../Assets/Images/nelsonMandela.png";
+import steveJobs from "../Assets/Images/steveJobs.png";
+
+export const celebrities = [
+{
+    title : "Nelson Mandela",
+    image : nelsonMandela,
+    description : "Nelson Mandela was an outstanding leader of African nationalism and a professional lawyer born on July 18, 1918, in South Africa. ",
+    slideNumber : 1,
+},
+
+{
+    title : "APJ Abdul Kalam",
+    image : apj,
+    description : "Avul Pakir Jainulabdeen Abdul Kalam BR was an Indian aerospace scientist and statesman who served as the 11th president of India from 2002 to 2007. ",
+    slideNumber : 2,
+},
+
+
+{
+    title : "Albert Einstien",
+    image : einstein,
+    description : "Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.",
+    slideNumber : 3,
+},
+
+
+{
+    title : "Billgates",
+    image : billGates,
+    description : "William Henry Gates III is an American businessman, investor, philanthropist, and writer best known for co-founding the software giant Microsoft, along with his childhood friend Paul Allen. ",
+    slideNumber : 4,
+},
+
+{
+    title : "Steve Jobs",
+    image : steveJobs,
+    description : "Steven Paul Jobs was an American businessman, inventor, and investor best known for co-founding the technology giant Apple Inc.",
+    slideNumber : 5,
+},
+
+
+]
